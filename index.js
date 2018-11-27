981f6d31ebdd1e1ca26587d55faac9e433636635
Ally Netzel <netzelally@gmail.com>
2018-11-27 22:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+    const createFareMultiplier= function(fare) {
+        return function(num) {
+            return num * fare;
+        };
+    };
+    
+     
+     
