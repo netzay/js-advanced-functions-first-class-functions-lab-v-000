bf4ac5ec97243b9c5f997703208a26857b64afaf
Ally Netzel <netzelally@gmail.com>
2018-11-27 22:58:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
     
      
      const fareDoubler = function(fare){
-       return createFareMultiplier(fare); {
+       return createFareMultiplier(fare)(2); {
          return fare * 2;
         };
      };
